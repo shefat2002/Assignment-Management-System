2562a2802e86046fa30586e180ace40503c54596
shefat2002 <shefat2002@gmail.com>
2026-08-05 20:19:21 +0600

    added swagger UI and fixed postgres bug

--- backend/AssignmentSystem.Api/Program.cs
+++ backend/AssignmentSystem.Api/Program.cs
@@ -7,29 +7,47 @@ using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
 Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Load connection string from environment (set by .env)
+var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection")
+    ?? Environment.GetEnvironmentVariable("DEFAULT_CONNECTION")
+    ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
+
+// Load JWT from environment
+var jwtKey = Environment.GetEnvironmentVariable("JWT_KEY") ?? builder.Configuration["Jwt:Key"];
+var jwtIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER") ?? builder.Configuration["Jwt:Issuer"];
+var jwtAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE") ?? builder.Configuration["Jwt:Audience"];
+
+builder.Services.AddSingleton(new
+{
+    Key = jwtKey,
+    Issuer = jwtIssuer,
+    Audience = jwtAudience
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-builder.Services.AddOpenApi();
-
-// builder.Services.AddValidatorsFromAssemblyContaining();
 builder.Services.AddFluentValidationAutoValidation();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.MapOpenApi();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "AssignmentSystem API v1");
+    c.RoutePrefix = "docs";
+});
 
 app.UseHttpsRedirection();
-
 app.UseAuthentication();
 app.UseAuthorization();
-app.MapControllers();
 
+app.MapGet("/", () => "Assignment System API");
+app.MapControllers();
 
 app.Run();
