eaf3dd922cae3c3dde99b8d3bb3da93a05422004
shefat2002 <shefat2002@gmail.com>
2026-08-06 09:07:56 +0600

    assignment creation dto

---
+++ backend/AssignmentSystem.Api/DTOs/Teacher/CreateAssignmentDto.cs
@@ -0,0 +1,15 @@
+using AssignmentSystem.Api.Models.Enitites;
+
+namespace AssignmentSystem.Api.DTOs.Teacher;
+
+public class CreateAssignmentDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime DueDate { get; set; }
+    public decimal TotalMarks { get; set; }
+    public bool AllowResubmission { get; set; }
+    public int ClassId { get; set; }
+    public int SubjectId { get; set; }
+    
+}
---
+++ backend/AssignmentSystem.Api/DTOs/Teacher/Validators/CreateAssignmentDtoValidator.cs
@@ -0,0 +1,29 @@
+using FluentValidation;
+
+namespace AssignmentSystem.Api.DTOs.Teacher.Validators;
+
+public class CreateAssignmentDtoValidator: AbstractValidator<CreateAssignmentDto>
+{
+    public  CreateAssignmentDtoValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Title is required")
+            .MaximumLength(100).WithMessage("Title can't be longer than 100 characters");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required")
+            .MaximumLength(4000).WithMessage("Description can't be longer than 4000 characters");
+
+        RuleFor(x => x.DueDate)
+            .GreaterThan(DateTime.Now).WithMessage("Due date must be in the future");
+
+        RuleFor(x => x.TotalMarks)
+            .GreaterThan(0).WithMessage("Total marks must be greater than 0");
+
+        RuleFor(x => x.ClassId)
+            .GreaterThan(0).WithMessage("A valid class id is required");
+
+        RuleFor(x => x.SubjectId)
+            .GreaterThan(0).WithMessage("A valid subject id is required");
+    }
+}
