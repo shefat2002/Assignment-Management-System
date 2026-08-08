e440ad1c1a80a1e2ae5c32fa79952a7b598ce954
shefat2002 <shefat2002@gmail.com>
2026-08-08 19:12:45 +0600

    Update Subject.cs

--- backend/AssignmentSystem.Api/Models/Enitites/Subject.cs
+++ backend/AssignmentSystem.Api/Models/Enitites/Subject.cs
@@ -4,5 +4,5 @@ public class Subject
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string? Description { get; set; }
+    public string? Description { get; set; }  = string.Empty;
 }
