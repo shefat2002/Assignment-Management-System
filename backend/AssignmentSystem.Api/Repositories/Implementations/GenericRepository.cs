99ec1083d9fa9398864c7bad535e077e55d194d5
shefat2002 <shefat2002@gmail.com>
2026-08-10 21:44:30 +0600

    implement generic repository that dynamically figureout the database table

---
+++ backend/AssignmentSystem.Api/Repositories/Implementations/GenericRepository.cs
@@ -0,0 +1,64 @@
+using System.Linq.Expressions;
+using AssignmentSystem.Api.Data;
+using AssignmentSystem.Api.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssignmentSystem.Api.Repositories.Implementations;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    private readonly AppDbContext _context;
+    private DbSet<T> _dbSet;
+
+    public GenericRepository(AppDbContext context, DbSet<T> dbSet)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<T?> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.Where(predicate).ToListAsync();
+    }
+
+    public async Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.FirstOrDefaultAsync(predicate);
+    }
+
+    public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.AnyAsync(predicate);
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+
+    public void Update(T entity)
+    {
+        _dbSet.Update(entity);
+    }
+
+    public void Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
