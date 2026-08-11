d868e3c00eb50437babb006637f6cdfe7285d22d
shefat2002 <shefat2002@gmail.com>
2026-08-11 08:40:22 +0600

    added update dtos

--- backend/AssignmentSystem.Api/DTOs/Teacher/GradeSubmissionDto.cs
+++ backend/AssignmentSystem.Api/DTOs/Teacher/GradeSubmissionDto.cs
@@ -1,7 +1,10 @@
+using AssignmentSystem.Api.Models.Enums;
+
 namespace AssignmentSystem.Api.DTOs.Teacher;
 
 public class GradeSubmissionDto
 {
     public decimal MarksAwarded { get; set; }
     public string? Feedback { get; set; } = string.Empty;
+    public SubmissionStatus Status { get; set; }
 }
