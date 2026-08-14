197b655e51aa67bb20d686f16c7fa092de6833fd
shefat2002 <shefat2002@gmail.com>
2026-08-14 20:33:42 +0600

    update dtos

--- backend/AssignmentSystem.Api/DTOs/Admin/CreateClassDto.cs
+++ backend/AssignmentSystem.Api/DTOs/Admin/CreateClassDto.cs
@@ -3,5 +3,7 @@ namespace AssignmentSystem.Api.DTOs.Admin;
 public class CreateClassDto
 {
     public string Name { get; set; } =  string.Empty;
+    public string Section { get; set; } = string.Empty;
+    public int Year { get; set; }
     public string? Description { get; set; } =  string.Empty;
 }
