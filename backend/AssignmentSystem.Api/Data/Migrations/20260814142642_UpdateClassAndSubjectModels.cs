ec7193182a699c5053c101e9a9e7bfc7d3c0068b
shefat2002 <shefat2002@gmail.com>
2026-08-14 20:29:58 +0600

    add new migration

---
+++ backend/AssignmentSystem.Api/Data/Migrations/20260814142642_UpdateClassAndSubjectModels.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssignmentSystem.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateClassAndSubjectModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClassId",
+                table: "Subjects",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Subjects",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Section",
+                table: "Classes",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Year",
+                table: "Classes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_ClassId",
+                table: "Subjects",
+                column: "ClassId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subjects_Classes_ClassId",
+                table: "Subjects",
+                column: "ClassId",
+                principalTable: "Classes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subjects_Classes_ClassId",
+                table: "Subjects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Subjects_ClassId",
+                table: "Subjects");
+
+            migrationBuilder.DropColumn(
+                name: "ClassId",
+                table: "Subjects");
+
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Subjects");
+
+            migrationBuilder.DropColumn(
+                name: "Section",
+                table: "Classes");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Classes");
+        }
+    }
+}
